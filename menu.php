99989208c822295a6c12f1ec44eeff900333b8b8
Alexandra Tamas <alexandra.tamas92@yahoo.com>
2017-01-21 19:01:31 +0100

    Schokoladenarten und Größen umgesetzt
    Schokoladenarten umgesetzt
    Schokoladengrößen umgesetzt
    Navigation verfeinert
    Menü in eigene Datei geschrieben

---
+++ menu.php
@@ -0,0 +1,41 @@
+<div class="header-sec">
+    <div class="header-row w-row">
+      <div class="column1 w-col w-col-10 w-col-small-small-stack">
+        <div class="navbar w-nav" data-animation="default" data-collapse="medium" data-duration="400">
+          <div class="w-container">
+            <a class="logo w-nav-brand" href="index.php"></a>
+            <nav class="navmenumobile w-nav-menu" role="navigation"><a class="menuitem w-nav-link" href="index.php">Startseite</a>
+              <div class="w-dropdown" data-delay="0" data-hover="1">
+                <div class="menuitem w-dropdown-toggle">
+                  <div>schkoloadegrössen</div>
+                  <div class="w-icon-dropdown-toggle"></div>
+                </div>
+                <nav class="dropdownlist w-dropdown-list"><a class="dropdownmenulink w-dropdown-link" href="schokoladengroen.php?groesse=Klein">klein</a><a class="dropdownmenulink w-dropdown-link" href="schokoladengroen.php?groesse=Mittel">mittel</a><a class="dropdownmenulink w-dropdown-link" href="schokoladengroen.php?groesse=Gross">gross</a>
+                </nav>
+              </div>
+              <div class="w-dropdown" data-delay="0" data-hover="1">
+                <div class="menuitem w-dropdown-toggle">
+                  <div>Schokoladearten</div>
+                  <div class="w-icon-dropdown-toggle"></div>
+                </div>
+                <nav class="dropdownlist w-dropdown-list"><a class="dropdownmenulink w-dropdown-link" href="schokoladenarten.php?art=Zartbitter">zartbitter</a><a class="dropdownmenulink w-dropdown-link" href="schokoladenarten.php?art=Vollmilch">vollmilch</a><a class="dropdownmenulink w-dropdown-link" href="schokoladenarten.php?art=Nougat">nougat</a>
+                </nav>
+              </div>
+              <a class="menuitem w-nav-link warenkorb" href="warenkorb.php"></a>
+            </nav>
+            <div class="menubutton w-nav-button">
+              <div class="menuicon w-icon-nav-menu"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="column2 w-col w-col-2 w-col-small-small-stack">
+        <a class="facebook header-link w-inline-block" href="http://twitter.com/schokischenk" target="_blank"><img class="social-img" height="20" src="images/twitter.png" width="20">
+        </a>
+        <a class="facebook header-link w-inline-block" href="http://instagram.com/schokischenk" target="_blank"><img class="social-img" height="20" src="images/instagram.png" width="20">
+        </a>
+        <a class="facebook header-link w-inline-block" href="http://facebook.com/schokischenk" target="_blank"><img class="social-img" height="20" src="images/facebook.png" width="20">
+        </a>
+      </div>
+    </div>
+  </div>
--- schokoladenarten.php
+++ schokoladenarten.php
@@ -1,3 +1,4 @@
+<?php require( "datenbank.php"); ?>
 <!DOCTYPE html>
 <!--  This site was created in Webflow. http://www.webflow.com -->
 <!--  Last Published: Wed Jan 11 2017 23:34:30 GMT+0000 (UTC)  -->
@@ -28,122 +29,68 @@
   <link href="images/psiusafari.png" rel="apple-touch-icon">
 </head>
 <body>
-  <div class="header-sec">
-    <div class="header-row w-row">
-      <div class="column1 w-col w-col-10 w-col-small-small-stack">
-        <div class="navbar w-nav" data-animation="default" data-collapse="medium" data-duration="400">
-          <div class="w-container">
-            <a class="logo w-nav-brand" href="index.html"></a>
-            <nav class="navmenumobile w-nav-menu" role="navigation"><a class="menuitem w-nav-link" href="index.html">Startseite</a>
-              <div class="w-dropdown" data-delay="0" data-hover="1">
-                <div class="menuitem w-dropdown-toggle">
-                  <div>schkoloadegrössen</div>
-                  <div class="w-icon-dropdown-toggle"></div>
-                </div>
-                <nav class="dropdownlist w-dropdown-list"><a class="dropdownmenulink w-dropdown-link" href="schokoladengroen.html">klein</a><a class="dropdownmenulink w-dropdown-link" href="schokoladengroen.html">mittel</a><a class="dropdownmenulink w-dropdown-link" href="schokoladengroen.html">gross</a>
-                </nav>
-              </div>
-              <div class="w-dropdown" data-delay="0" data-hover="1">
-                <div class="menuitem w-dropdown-toggle">
-                  <div>Schokoladearten</div>
-                  <div class="w-icon-dropdown-toggle"></div>
-                </div>
-                <nav class="dropdownlist w-dropdown-list"><a class="dropdownmenulink w-dropdown-link" href="schokoladenarten.html">zartbitter</a><a class="dropdownmenulink w-dropdown-link" href="schokoladenarten.html">vollmilch</a><a class="dropdownmenulink w-dropdown-link" href="schokoladenarten.html">nougat</a>
-                </nav>
-              </div>
-              <a class="menuitem w-nav-link warenkorb" href="warenkorb.html"></a>
-            </nav>
-            <div class="menubutton w-nav-button">
-              <div class="menuicon w-icon-nav-menu"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="column2 w-col w-col-2 w-col-small-small-stack">
-        <a class="facebook header-link w-inline-block" href="http://twitter.com/schokischenk" target="_blank"><img class="social-img" height="20" src="images/twitter.png" width="20">
-        </a>
-        <a class="facebook header-link w-inline-block" href="http://instagram.com/schokischenk" target="_blank"><img class="social-img" height="20" src="images/instagram.png" width="20">
-        </a>
-        <a class="facebook header-link w-inline-block" href="http://facebook.com/schokischenk" target="_blank"><img class="social-img" height="20" src="images/facebook.png" width="20">
-        </a>
-      </div>
-    </div>
-  </div>
+  <?php include("menu.php"); ?>
+    <?php
+if( isset( $_GET["art"] ) && !empty( $_GET["art"] ) ) {
+  $groesse = $_GET["art"];
+  $sql = "SELECT * FROM Gut WHERE schokoladenart LIKE '" . $groesse . "'";
+
+  $result = $conn->query( $sql );
+  
+  if( $result->num_rows <= 0 ) {
+    echo "Ich habe keine Produkte gefunden";
+  }
+
+  else {
+    $alleSchokoladen = $result->fetch_all();
+
+    shuffle( $alleSchokoladen );
+    $counter = 0;
+
+     ?>
   <div class="sectionchocolatesize">
-    <div class="w-container">
-      <h1 class="headingangebot">Unsere walnuss - schokoladen</h1>
+    <div class="w-container" style="margin-bottom:15%;">
+      <h1 class="headingangebot">Unsere <?php echo $groesse; ?> - schokoladen</h1>
       <h5 class="secondsectionheading5">Solange der Vorracht reicht.</h5>
-      <div class="angeboterow w-row">
-        <div class="columninsiderow w-col w-col-4">
-          <div class="imagediv"><img src="images/test.png">
-            <h4 class="nameofproduct">Supergeile Schokolade frisch gemacht aus kuhscheisse</h4>
-            <div class="productinformation">
-              <div class="chocolatesize">Art: Walnuss</div>
-              <div class="chocolatesize">Grösse: Gross</div>
-              <h4 class="pricetag">50€<br><em>inkl.Mwst.</em></h4>
-              <div class="shippingtag">zzgl. Versand und Bezahlkosten</div><a class="verfgbarkeitbtn w-button" href="schokolade.html">Jetzt Verfügbarkeit prüfen</a>
-            </div>
-          </div>
-        </div>
-        <div class="columninsiderow w-col w-col-4">
-          <div class="imagediv"><img src="images/test.png">
-            <h4 class="nameofproduct">Supergeile Schokolade frisch gemacht aus kuhscheisse</h4>
-            <div class="productinformation">
-              <div class="chocolatesize">Art: Walnuss</div>
-              <div class="chocolatesize">Grösse: Gross</div>
-              <h4 class="pricetag">50€<br><em>inkl.Mwst.</em></h4>
-              <div class="shippingtag">zzgl. Versand und Bezahlkosten</div><a class="verfgbarkeitbtn w-button" href="schokolade.html">Jetzt Verfügbarkeit prüfen</a>
-            </div>
-          </div>
-        </div>
-        <div class="columninsiderow w-col w-col-4">
-          <div class="imagediv"><img src="images/test.png">
-            <h4 class="nameofproduct">Supergeile Schokolade frisch gemacht aus kuhscheisse</h4>
-            <div class="productinformation">
-              <div class="chocolatesize">Art: Walnuss</div>
-              <div class="chocolatesize">Grösse: Gross</div>
-              <h4 class="pricetag">50€<br><em>inkl.Mwst.</em></h4>
-              <div class="shippingtag">zzgl. Versand und Bezahlkosten</div><a class="verfgbarkeitbtn w-button" href="schokolade.html">Jetzt Verfügbarkeit prüfen</a>
-            </div>
-          </div>
-        </div>
+      
+
+<?php 
+
+    foreach( $alleSchokoladen as $singleSchokolade ) {
+
+      if( $counter % 3 == 0 ) {
+
+        if( $counter >= 3  ) {
+          $secondrow = 'secondrow ';
+        }
+        else {
+          $secondrow = '';
+        }
+        echo '<div class="angeboterow ' .$secondrow . 'w-row">';
+      }
+      echo '<div class="columninsiderow w-col w-col-4">';
+      echo '<div class="imagediv"><img src="images/' . $singleSchokolade[0] . '.jpg">';
+      echo '<h4 class="nameofproduct">' . $singleSchokolade[3] . '</h4>';
+      echo '<div class="productinformation">';
+      echo '<div class="chocolatesize">Art: ' . $singleSchokolade[1] . '</div>';
+      echo '<div class="chocolatesize">Grösse: ' . $singleSchokolade[2] . '</div>';
+      echo '<h4 class="pricetag">' . $singleSchokolade[4] . '€<br><em>inkl.Mwst.</em></h4>';
+      echo '<div class="shippingtag">zzgl. Versand und Bezahlkosten</div><a class="verfgbarkeitbtn w-button" href="schokolade.php?id=' . $singleSchokolade[0] . '">Jetzt Verfügbarkeit prüfen</a>';
+      echo '</div>';
+      echo '</div>';
+      echo '</div>';
+
+      if( $counter %3 == 2 ) {
+        echo '</div>';
+      }
+      $counter++;
+    }
+  }
+}
+?>
+
+
       </div>
-      <div class="angeboterow secondrow w-row">
-        <div class="columninsiderow w-col w-col-4">
-          <div class="imagediv"><img src="images/test.png">
-            <h4 class="nameofproduct">Supergeile Schokolade frisch gemacht aus kuhscheisse</h4>
-            <div class="productinformation">
-              <div class="chocolatesize">Art: Walnuss</div>
-              <div class="chocolatesize">Grösse: Gross</div>
-              <h4 class="pricetag">50€<br><em>inkl.Mwst.</em></h4>
-              <div class="shippingtag">zzgl. Versand und Bezahlkosten</div><a class="verfgbarkeitbtn w-button" href="schokolade.html">Jetzt Verfügbarkeit prüfen</a>
-            </div>
-          </div>
-        </div>
-        <div class="columninsiderow w-col w-col-4">
-          <div class="imagediv"><img src="images/test.png">
-            <h4 class="nameofproduct">Supergeile Schokolade frisch gemacht aus kuhscheisse</h4>
-            <div class="productinformation">
-              <div class="chocolatesize">Art: Walnuss</div>
-              <div class="chocolatesize">Grösse: Gross</div>
-              <h4 class="pricetag">50€<br><em>inkl.Mwst.</em></h4>
-              <div class="shippingtag">zzgl. Versand und Bezahlkosten</div><a class="verfgbarkeitbtn w-button" href="schokolade.html">Jetzt Verfügbarkeit prüfen</a>
-            </div>
-          </div>
-        </div>
-        <div class="columninsiderow w-col w-col-4">
-          <div class="imagediv"><img src="images/test.png">
-            <h4 class="nameofproduct">Supergeile Schokolade frisch gemacht aus kuhscheisse</h4>
-            <div class="productinformation">
-              <div class="chocolatesize">Art: Walnuss</div>
-              <div class="chocolatesize">Grösse: Gross</div>
-              <h4 class="pricetag">50€<br><em>inkl.Mwst.</em></h4>
-              <div class="shippingtag">zzgl. Versand und Bezahlkosten</div><a class="verfgbarkeitbtn w-button" href="schokolade.html">Jetzt Verfügbarkeit prüfen</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
   <div class="section-footer warenkorbfooter">
     <div class="w-container">
