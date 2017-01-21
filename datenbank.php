a960a65b39eb39d19d7bffc217aaea9981eb6c58
Alexandra Tamas <alexandra.tamas92@yahoo.com>
2017-01-22 00:07:13 +0100

    Datenbank update
    für remote Datenbank, Anbindung an Veronika uni wien Datenbank

--- datenbank.php
+++ datenbank.php
@@ -1,12 +1,17 @@
 <?php
+/* Lokal
 $servername = "localhost";
 $username = "root";
 $password = "root";
 $datenbank = "schokischenk";
+*/
+$servername = "a0849065.mysql.univie.ac.at";
+$username = "a0849065";
+$password = "datenbank1";
+$datenbank= "a0849065";
 $conn = mysqli_connect( $servername, $username, $password, $datenbank );
 
 if( !$conn ) { 
 	die( 'Konnte keine Verbindung aufbauen' . mysqli_connect_error() );
 }
-
 ?>
