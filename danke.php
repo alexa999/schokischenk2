ce0d816d6af0d9628a79a4c0c128cb78ec173de9
veronika <a0849065@unet.univie.ac.at>
2017-01-23 22:05:04 +0100

    Add files via upload

---
+++ danke.php
@@ -0,0 +1,70 @@
+<?php require( 'datenbank.php' );
+    $_SESSION["email"] = "lisa@muster.at";
+    ?>
+<!DOCTYPE html>
+<!--  This site was created in Webflow. http://www.webflow.com -->
+<!--  Last Published: Wed Jan 11 2017 23:34:30 GMT+0000 (UTC)  -->
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Schokischenk OnlineShop</title> 
+  <meta content="Danke für Ihre Bestellung!" name="description">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta content="Webflow" name="generator">
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/webflow.css" rel="stylesheet" type="text/css"> 
+  <link href="css/alexandras-fantabulous-site-1de408.webflow.css" rel="stylesheet" type="text/css">
+  <script src="js/webfont.js"></script>
+  <script type="text/javascript">
+    WebFont.load({
+      google: {
+        families: ["Inconsolata:400,400italic,700,700italic","Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic"]
+      }
+    });
+  </script>
+  <script src="js/epe5bdv.js" type="text/javascript"></script>
+  <script type="text/javascript">
+    try{Typekit.load();}catch(e){}
+  </script>
+  <script src="js/modernizr.js" type="text/javascript"></script> 
+</head>
+<body>
+  <div class="top-linking" id="top">
+<?php include("menu.php"); ?>
+  </div>
+  <div class="featured w-slider" data-animation="fade" data-autoplay="1" data-delay="5000" data-duration="5000" data-infinite="1" data-ix="fetured-load">
+    <div class="w-slider-mask">
+      <div class="featured-slide1 w-slide"></div>
+      <div class="featured-slide2 w-slide">
+        <div class="w-row">
+          <div class="w-col w-col-6"></div>
+          <div class="w-col w-col-6"></div>
+        </div>
+      </div>
+      <div class="featured-slide3 w-slide"></div>
+      <div class="featured-slide4 w-slide"></div>
+    </div>
+  </div>
+  <div class="text-container" data-ix="header-load">
+    <h2 class="ueberschrift2">Danke für Ihre Bestellung</h2>
+
+  </div>
+    <div class="section-footer warenkorbfooter">
+    <div class="w-container">
+      <div class="w-row">
+        <div class="made-column w-col w-col-6 w-col-medium-6 w-col-small-6">
+          <div class="made-text">Made in Digitale Ökönomie</div>
+        </div>
+        <div class="keda-logo-column w-col w-col-6 w-col-medium-6 w-col-small-6">
+          <div class="copyright-text">© 2016 SCHOKISCHENK @ UNI WIEN</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" type="text/javascript"></script>
+  <script src="js/webflow.js" type="text/javascript"></script>
+  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+  <script src="http://psiupuxa.com/js/particles.js"></script>
+  <script src="http://psiupuxa.com/js/app.js"></script>
+</body>
+</html>
